8ec9cf02a003eb371c9ff9c95a43d9e059bb3145
Maxim Pyatkin <pyatkinmv@gmail.com>
2021-10-24 18:38:32 +0300

    Fix test class name

--- src/test/java/ru/pyatkinmv/service/AnswerServiceTest.java
+++ src/test/java/ru/pyatkinmv/service/AnswerServiceTest.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = QuizApplication.class)
-class QuestionServiceTest {
+class AnswerServiceTest {
     @Autowired
     private AnswerService answerService;
 
